2d41114d7365e17c08388bce4098b5a931c85267
Prasanth <prasanthshanmugam1402@gmail.com>
2025-09-29 20:23:35 +0100

    Add project files.

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,73 @@
+using System.Net;
+using System.Net.Http;
+using System.Diagnostics;
+
+namespace BetaSurf
+{
+    public partial class BetaSurf : Form
+    {
+        public BetaSurf()
+        {
+            InitializeComponent();
+        }
+
+        private void backwardClick(object sender, EventArgs e)
+        {
+
+        }
+        private void forwardClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void reloadButtonClick(object sender, EventArgs e)
+        {
+
+        }
+        private void searchTextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //SAMPLE DEBUG STATEMENT
+        //Debug.WriteLine("Sender Object -> " + searchText);
+        //Debug.WriteLine("E - > " + e);
+
+
+        private async void searchButtonClick(object sender, EventArgs e)
+        {
+
+            // fetching the user input from search box
+            String searchText = searchBox.Text;
+
+            // using httpClient library to manage the HTTP calls
+            using HttpClient client = new HttpClient();
+            
+            //making a google search of whatever the user enters
+            HttpResponseMessage response = await client.GetAsync("https://www.google.com/search?q=" + searchText);
+
+            //storing the user response in a variable
+            HttpStatusCode responseCode = response.StatusCode;
+            
+            //storing the Raw HTML 
+            String rawHTML = await response.Content.ReadAsStringAsync();
+
+            //displaying the Status in separate text box 
+            displayCodeBox.Text = response.StatusCode.ToString();
+
+            // displaying the Raw HTML
+            displayTextBox.Text = rawHTML;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WinFormsApp1/Service.cs
@@ -0,0 +1,23 @@
+﻿using System.ServiceProcess;
+
+namespace BetaSurf
+{
+    partial class Service : ServiceBase
+    {
+        public Service()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // TODO: Add code here to start your service.
+        }
+
+        protected override void OnStop()
+        {
+            // TODO: Add code here to perform any tear-down necessary to stop your service.
+        }
+
+    }
+}
